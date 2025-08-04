2bef7627f0697c86229c93722f5c6370bdf53136
Pablo <pablocasamianestudios@gmail.com>
2025-08-04 14:45:57 +0200

    game

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-    
+  <div>
+    <button @click="goToGame">Ir a Game Scene</button>
     <router-view />
-
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'App',
+  name: 'App',
+  methods: {
+    goToGame() {
+      this.$router.push('/game');
+    }
+  }
 }
 </script>
 
